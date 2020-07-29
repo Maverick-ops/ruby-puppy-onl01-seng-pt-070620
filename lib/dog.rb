cf348f40317b8cdca77d6180ffdce516b994a1a1
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-29 23:23:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize(name)
     @name = name
-   # @@all << self #everything passed with this until the end not sure why 
+   @@all << self #everything passed with this until the end not sure why 
    #i had to remove this and add save
    save  
   end 
